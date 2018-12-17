f70fb9037dda22c3d903e6756cc88c8df316a44d
lijf <lijf.hz@outlook.com>
2018-12-18 02:03:38 +0800

    readme

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = logo
 	app.Version = "v0.1.0"
-	app.Usage = "A cli todo list application"
+	app.Usage = "A cli todo list app in Golang."
 	app.Author = "lijf93"
 	app.Email = "lijf93.hz@outlook.com"
 	app.Commands = []cli.Command{}
